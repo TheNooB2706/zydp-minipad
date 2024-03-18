b21cd351bb92575acb369a8351224de7af06f0d2
TheNooB <noobchannel2706@gmail.com>
2024-03-18 21:21:51 +0800

    Added CC code

---
+++ lib/CController/src/ccontroller.cpp
@@ -0,0 +1,47 @@
+#include "ccontroller.hpp"
+#include <Arduino.h>
+#include <cmath>
+
+CController::CController(int pin_num, float threshold_percent, int midi_cc_num) {
+    pin = pin_num;
+    pinMode(pin, INPUT);
+
+    _last_reading = analogRead(pin);
+
+    _delta_threshold = round(4096*threshold_percent/100);
+
+    _midi_cc_num = midi_cc_num;
+}
+
+CController::CController(int pin_num, float threshold_percent) : CController(pin_num, threshold_percent, 0) {}
+
+int CController::poll() {
+    int cur_reading = analogRead(pin);
+    if (abs(cur_reading-_last_reading) > _delta_threshold) {
+        on_change(cur_reading);
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int CController::poll(int sample_period_micro) {
+    if ((micros()-_last_sample_time) > sample_period_micro) {
+        _last_sample_time = micros();
+        return poll();
+    }
+    else {
+        return 0;
+    }    
+}
+
+int CController::get_cc_num() {
+    return _midi_cc_num;
+}
+
+void CController::set_cc_num(int new_cc_num) {
+    _midi_cc_num = new_cc_num;
+}
+
+void CController::on_change(int controller_input) {}
---
+++ lib/CController/src/ccontroller.hpp
@@ -0,0 +1,31 @@
+#include <stdint.h>
+
+class CController {
+    public:
+
+        uint8_t pin;
+
+        CController(int pin_num, float threshold_percent);
+        CController(int pin_num, float threshold_percent, int midi_cc_num);
+
+        int poll();
+
+        int poll(int sample_period_micro);
+
+        /// @brief Get MIDI CC number assigned to this pad.
+        int get_cc_num();
+
+        /// @brief Assign new MIDI CC number to this pad.
+        void set_cc_num(int new_cc_num);
+
+        // Overload the following function to be executed in poll when controller changed.
+        virtual void on_change(int controller_input);
+
+    private:
+
+        int _last_reading;
+        int _delta_threshold;
+        uint32_t _last_sample_time = 0;
+        int _midi_cc_num;
+
+};
